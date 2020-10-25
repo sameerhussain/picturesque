0e40eb23a3645169d083eb660c2d17644e347adf
sameer hussain <sameerhussain194@gmail.com>
2020-10-25 08:59:02 +0530

    Added skeletal structure

---
+++ src/main/java/com/picturesque/randompictures/RandompicturesApplication.java
@@ -0,0 +1,13 @@
+package com.picturesque.randompictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandompicturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandompicturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picturesque/randompictures/RandompicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picturesque.randompictures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandompicturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
